9a0f983ffe34b5dcab2acebf0005e07d55665eef
gcland <grantbcopeland@gmail.com>
2024-09-30 11:04:22 -0500

    Initial upload Mod10Les01 Assignment
    Added item 1 tasks 1 - 3 and item 2 tasks 1 - 6

---
+++ script.js
@@ -0,0 +1,64 @@
+// Mod10Les01 
+
+// Understanding Data Types in JavaScript
+
+// Task 1
+
+let studentName
+let studentAge
+let studentGrade
+let isPresent
+
+// Task 2
+
+studentName = "Alice";
+studentAge = 15;
+studentGrade = 10;
+isPresent = true;
+
+// Task 3
+
+console.log("Student name: " + studentName);
+console.log("Student age: " + studentAge);
+console.log("Student grade: " + studentGrade);
+console.log("Student present: " + isPresent);
+console.log()
+//------------------------------------------------
+
+//Exploring JavaScript Operators
+
+// Task 1 and 2
+
+let num1 = 5
+let num2 = 10
+
+// Task 3
+
+console.log("Sum: " + (num1 + num2));
+console.log("Difference: " + (num1 - num2));
+console.log("Product: " + (num1*num2));
+console.log("Quotient: " + (num1/num2));
+console.log()
+
+// Task 4
+
+num1 = 4
+num2 = 1
+
+console.log("Updated num1: " + num1);
+console.log("Updated num2: " + num2);
+console.log()
+
+// Task 5
+
+console.log("Is num1 equal to num2?", num1 == num2);
+console.log("Is num1 greater than num2?", (num1 > num2));
+console.log("Is num1 not equal to num2?", num1 != num2);
+console.log()
+
+// Task 6 
+
+let isPositive = num1 && num2 > 0
+let isEven = num1%2 == 0 || num2%2 == 0;
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
